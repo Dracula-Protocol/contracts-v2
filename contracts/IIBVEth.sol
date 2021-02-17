95b262e56867c8e84bf3536e7b6cf532f5369a53
Aaron Zinghini <aaron.zinghini@seeingmachines.com>
2021-02-17 12:58:20 -0700

    Refactor interest bearing ETH logic into common interface

---
+++ contracts/IIBVEth.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+/**
+* @title Interface for interest bearing ETH strategies
+*/
+abstract contract IIBVEth  {
+
+    function handleDrainedWETH(uint256 amount) external virtual;
+
+    function handleClaim(uint256 pending, uint8 flag) external virtual;
+
+    function _safeETHTransfer(address payable to, uint256 amount) internal virtual {
+        uint256 balance = address(this).balance;
+        if (amount > balance) {
+            to.transfer(balance);
+        } else {
+            to.transfer(amount);
+        }
+    }
+}
