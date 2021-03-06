347422a8497b8f491b1e12f2cee33ed3e3c30e8d
Aaron <aaronovz@protonmail.com>
2021-03-06 14:20:11 -0700

    Prepare build environment to use hardhat-deploy and code coverage tools

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
+pragma solidity ^0.7.6;
 
-import '../interfaces/IUniswapV2Pair.sol';
-
+import "../interfaces/IUniswapV2Pair.sol";
 import "./SafeMath.sol";
 
 library UniswapV2Library {
