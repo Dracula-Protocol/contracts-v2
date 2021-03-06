1638f6be09bff463a576e35168ebdf3acdf287f0
Aaron Zinghini <aaron.zinghini@seeingmachines.com>
2021-03-06 14:20:11 -0700

    Prepare build environment to use hardhat-deploy and code coverage tools

--- contracts/strategies/IBVEthRari.sol
+++ contracts/strategies/IBVEthRari.sol
@@ -15,14 +15,14 @@ import "../IIBVEth.sol";
 contract IBVEthRari is IIBVEth, IMasterVampire {
     using SafeMath for uint256;
 
-    IRariFundManager constant FUND_MANAGER = IRariFundManager(0xD6e194aF3d9674b62D1b30Ec676030C23961275e);
-    IUniswapV2Pair immutable DRC_WETH_PAIR;
-    IERC20 immutable dracula;
+    IRariFundManager private constant FUND_MANAGER = IRariFundManager(0xD6e194aF3d9674b62D1b30Ec676030C23961275e);
+    IUniswapV2Pair private immutable drcWethPair;
+    IERC20 private immutable dracula;
 
     constructor(address _dracula) {
         dracula = IERC20(_dracula);
         IUniswapV2Factory uniswapFactory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
-        DRC_WETH_PAIR = IUniswapV2Pair(uniswapFactory.getPair(address(WETH), _dracula));
+        drcWethPair = IUniswapV2Pair(uniswapFactory.getPair(address(WETH), _dracula));
     }
 
     function handleDrainedWETH(uint256 amount) external override {
@@ -43,19 +43,20 @@ contract IBVEthRari is IIBVEth, IMasterVampire {
             _safeETHTransfer(msg.sender, pending);
         } else {
             WETH.deposit{value: pending}();
-            address token0 = DRC_WETH_PAIR.token0();
-            (uint reserve0, uint reserve1,) = DRC_WETH_PAIR.getReserves();
+            address token0 = drcWethPair.token0();
+            (uint reserve0, uint reserve1,) = drcWethPair.getReserves();
             (uint reserveInput, uint reserveOutput) = address(WETH) == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
             uint amountOutput = UniswapV2Library.getAmountOut(pending, reserveInput, reserveOutput);
             (uint amount0Out, uint amount1Out) = address(WETH) == token0 ? (uint(0), amountOutput) : (amountOutput, uint(0));
 
-            WETH.transfer(address(DRC_WETH_PAIR), pending);
-            DRC_WETH_PAIR.swap(amount0Out, amount1Out, address(this), new bytes(0));
+            WETH.transfer(address(drcWethPair), pending);
+            drcWethPair.swap(amount0Out, amount1Out, address(this), new bytes(0));
             dracula.transfer(msg.sender, amountOutput);
         }
     }
 
-    function migrate() external override {
+    function migrate() external pure override {
+        require(false, "not implemented");
     }
 
     function ibToken() external view override returns(IERC20) {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,16 +2,17 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 dotenv.config({ path: __dirname + '/.env' });
 
-import "@nomiclabs/hardhat-ganache";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
 import "hardhat-typechain";
 import "hardhat-gas-reporter";
+import "solidity-coverage";
+
 
 const INFURA_API_KEY = `${process.env.INFURA_API_KEY}`;
-const RINKEBY_PRIVATE_KEY = `${process.env.RINKEBY_PRIVATE_KEY}`;
-const ROPSTEN_PRIVATE_KEY = `${process.env.ROPSTEN_PRIVATE_KEY}`;
 const KOVAN_PRIVATE_KEY = `${process.env.KOVAN_PRIVATE_KEY}`;
 const MAINNET_PRIVATE_KEY = `${process.env.MAINNET_PRIVATE_KEY}`;
 const ETHERSCAN_API_KEY = `${process.env.ETHERSCAN_API_KEY}`;
@@ -49,14 +50,6 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       url: "http://127.0.0.1:8546"
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`${RINKEBY_PRIVATE_KEY}`]
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [ROPSTEN_PRIVATE_KEY]
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
