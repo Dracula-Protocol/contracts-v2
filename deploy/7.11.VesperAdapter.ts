f1a772d0701a3721429d9da8a1d4808621270df1
mdzor <mdzor@protonmail.com>
2021-04-14 21:04:58 +0800

    fix MasterVampire address in deploy

--- deploy/7.11.VesperAdapter.ts
+++ deploy/7.11.VesperAdapter.ts
@@ -19,7 +19,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       from: deployer,
       log: true,
       contract: 'VesperAdapter',
-      args: [WETH, uniFactory, MasterVampire]
+      args: [WETH, uniFactory, MasterVampire.address]
     });
 
     const masterVampire = await ethers.getContractAt('MasterVampire', MasterVampire.address, ethers.provider.getSigner(deployer));
