73b8ffdc6e4bb232613fa93294b5faaec79119e7
Aaron <20787054+aaronovz1@users.noreply.github.com>
2021-05-28 08:59:52 -0700

    add yearn weth vault address

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -137,6 +137,9 @@ const config: HardhatUserConfig = {
     USDT: {
       1: '0xdAC17F958D2ee523a2206206994597C13D831ec7'
     },
+    yvWETHVault: {
+      1: '0xa9fE4601811213c340e850ea305481afF02f5b28'
+    },
     WETH: {
       1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     },
